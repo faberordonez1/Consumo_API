428029f5715eeb1cd57aa37d95264fa752784381
Faber Ordoñez <33733612+faberordonez1@users.noreply.github.com>
2021-08-24 23:11:40 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+function renderPokemon(data) {
+  //var contenedor = document.getElementsByTagName("main");
+  //contenedor.appendChild(element);
+
+  var template = document.getElementById("element");
+  var element = template.cloneNode(true);
+  var main = document.getElementsByTagName("main")[0];
+  main.appendChild(element);
+
+  var image = document.createElement("img");
+  image.src = data.sprites.front_default;
+  element.appendChild(image);
+
+  var h3 = document.createElement("h3");
+  h3.textContent = data.name;
+  element.appendChild(h3);
+}
+
+//https://pokeapi.co/api/v2/{endpoint}
+
+function getPokemon(url) {
+  fetch(url)
+    .then((response) => response.json()) //Respuesta a JSON
+    .then((data) => {
+      renderPokemon(data); //funcion que procesa la Rta
+      /*console.log(element);
+    element.innerHTML = `
+    <img src="${data.sprites.front_default}" class="poke_card_image">
+    <h3 class="poke_card_nombre" >${data.name}</h3>
+    <p class="poke_card_description" >Lorem ipsun dolor set</p>
+    `;*/
+    })
+    .catch((error) => console.log(error));
+}
+
+for (var i = 1; i > 5; i++) {
+  var url = "https://pokeapi.co/api/v2/pokemon/" + i + "/";
+  getPokemon(url);
+}

f70ad25492f000ff7232549a59e1ca4616745e0f
Faber Ordoñez <faberordonez1@gmail.com>
2021-11-07 14:01:44 -0500

    render poster & titulo

--- app.js
+++ app.js
@@ -2,51 +2,51 @@ const cargarPeliculas =  async() =>{
     //const url =  https://api.themoviedb.org/3/movie/550?api_key=e1ee3a585e3ff2efd9d0a990db15e813&language=es-MX
     const url = "https://api.themoviedb.org/3/movie/popular"
   try {
-    const respuesta = await fetch (url + "?api_key=e1ee3a585e3ff2efd9d0a990db15e813&language=es-MX");
+      const respuesta = await fetch (url + "?api_key=e1ee3a585e3ff2efd9d0a990db15e813&language=es-MX");
 
-    //console.log(respuesta);
-    //verifica codigo respuesta
-    if(respuesta.status ===200){
-      const datos = await respuesta.json();/*convertido a JSON*/
-      console.log(datos);
-    }else if (respuesta.status === 401){
-      console.log ("Pusiste la llave mal")
-    }else if(respuesta.status === 404){
-       console.log("La pelicula no existe");
-    }else{
-       console.log("Error, no sabemos que pasó")
-    }
+      //console.log(respuesta);
+      //verifica codigo respuesta
+      if(respuesta.status === 200){
+        const datos = await respuesta.json();/*convertido a JSON*/
+
+        datos.results.forEach(item => {/**recorre la rta */
+          renderMovies(item);
+        });
+
+        //console.log(datos);
+      }else if (respuesta.status === 401){
+        console.log ("Pusiste la llave mal")
+      }else if(respuesta.status === 404){
+        console.log("La pelicula no existe");
+      }else{
+        console.log("Error, no sabemos que pasó")
+      }
     
   } catch (error) {
     console.log(error);
-    
   }
 
 }
 cargarPeliculas();
 
 
-function renderPokemon(data) {
-
-  var main = document.getElementsByTagName("main")[0];/**Captura main contenedor */
+function renderMovies(datos) {
+  var contenedor = document.querySelector(".contenedor");/**Captura main contenedor */
   
   var card = document.createElement("div");
-  main.appendChild(card);
+  card.setAttribute('class','pelicula');
+  contenedor.appendChild(card);
 
   var image = document.createElement("img");
-  image.src = data.sprites.front_default;
+  image.setAttribute('class','poster');
+  const urlBase ="https://image.tmdb.org/t/p/w500/";
+  image.src = urlBase + datos.poster_path;
   card.appendChild(image);
 
   var h3 = document.createElement("h3");
-  h3.textContent = data.name;
+  h3.setAttribute('class','title');
+  h3.textContent = datos.title;
   card.appendChild(h3);
-  
-  /*console.log(element);
-  card.innerHTML = `
-  <img src="${data.sprites.front_default}" class="poke_card_image">
-  <h3 class="poke_card_nombre" >${data.name}</h3>
-  <p class="poke_card_description" >Lorem ipsun dolor set</p>
-  `;*/
 }
 
 function getPokemon(url) {
